f5c63309a9e0981f23e528d7b0ce91cdbbf6eed9
Peyton Bechard <peyton.bechard@gmail.com>
2024-05-27 18:46:49 +0800

    Adds averages row for chosen courses

--- src/BasicTable.js
+++ src/BasicTable.js
@@ -1,33 +1,43 @@
-import React, { useState, useEffect } from "react";
-import "./BasicTable.css"
+import React, { useState, useEffect } from 'react';
+import './BasicTable.css';
 
-function BasicTable({ tableId, rows, addToCourseList, showCheckbox, initiallySorted = true}) {
-  const [sortedRows, setSortedRows] = useState(rows)
-  const [currentlySortedBy, setCurrentlySortedBy] = useState("")
-  const [sortDirection, setSortDirection] = useState("ascending")
-
-  useEffect(() => {
-    if (initiallySorted) setCurrentlySortedBy("Course")
-  }, [])
-
-  useEffect(() => {
-    sortRows(currentlySortedBy)
-  }, [currentlySortedBy, sortDirection])
-
+function BasicTable({
+  tableId,
+  rows,
+  addToCourseList,
+  showCheckbox,
+  initiallySorted = true,
+}) {
+  const [sortedRows, setSortedRows] = useState(rows);
+  const [currentlySortedBy, setCurrentlySortedBy] = useState('');
+  const [sortDirection, setSortDirection] = useState('ascending');
 
   useEffect(() => {
-    sortRows(currentlySortedBy)
-  }, [rows.length])
+    if (initiallySorted) setCurrentlySortedBy('Course');
+  }, []);
+
+  useEffect(() => {
+    sortRows(currentlySortedBy);
+  }, [currentlySortedBy, sortDirection]);
+
+  useEffect(() => {
+    sortRows(currentlySortedBy);
+  }, [rows.length]);
 
   const handleHeaderClick = (event) => {
     const clickedHeader = event.target.childNodes[0]?.textContent.trim();
-    if (clickedHeader === currentlySortedBy || ["↑", "↓"].includes(clickedHeader)) {
-      setSortDirection(sortDirection === "ascending" ? "descending" : "ascending") }
-    else {
-      setSortDirection("ascending")
-      setCurrentlySortedBy(clickedHeader)
+    if (
+      clickedHeader === currentlySortedBy ||
+      ['↑', '↓'].includes(clickedHeader)
+    ) {
+      setSortDirection(
+        sortDirection === 'ascending' ? 'descending' : 'ascending'
+      );
+    } else {
+      setSortDirection('ascending');
+      setCurrentlySortedBy(clickedHeader);
     }
-  }
+  };
 
   const sortCompareFunction = (a, b, propertyName, direction) => {
     let aValue, bValue;
@@ -44,133 +54,272 @@ function BasicTable({ tableId, rows, addToCourseList, showCheckbox, initiallySor
     }
 
     if ((aValue || 0) < (bValue || 0)) {
-      return direction == "ascending" ? -1 : 1;
+      return direction == 'ascending' ? -1 : 1;
     }
     if ((aValue || 0) > (bValue || 0)) {
-      return direction == "ascending" ? 1 : -1;
+      return direction == 'ascending' ? 1 : -1;
     }
     return 0;
-  }
+  };
 
   const sortRows = (sortHeader) => {
     let propertyName;
-    if (sortHeader === "Course") { propertyName = "name" }
-    else if (sortHeader === "Foundational?") { propertyName = "isFoundational" }
-    else if (sortHeader === "Rating") { propertyName = "rating" }
-    else if (sortHeader === "Difficulty") { propertyName = "difficulty" }
-    else if (sortHeader === "Rating:Difficulty") { propertyName = "ratingDifficultyRatio" }
-    else if (sortHeader === "Workload") { propertyName = "workload" }
-    else if (sortHeader === "Rating:Workload") { propertyName = "ratingWorkloadRatio" }
-    else if (sortHeader === "Reviews") { propertyName = "reviewCount" }
-    else if (sortHeader === "Code(s)") { propertyName = "codes" }
-    setSortedRows(rows.toSorted((a, b) => sortCompareFunction(a, b, propertyName, sortDirection)))
-    setSortDirection(sortDirection)
-  }
+    if (sortHeader === 'Course') {
+      propertyName = 'name';
+    } else if (sortHeader === 'Foundational?') {
+      propertyName = 'isFoundational';
+    } else if (sortHeader === 'Rating') {
+      propertyName = 'rating';
+    } else if (sortHeader === 'Difficulty') {
+      propertyName = 'difficulty';
+    } else if (sortHeader === 'Rating:Difficulty') {
+      propertyName = 'ratingDifficultyRatio';
+    } else if (sortHeader === 'Workload') {
+      propertyName = 'workload';
+    } else if (sortHeader === 'Rating:Workload') {
+      propertyName = 'ratingWorkloadRatio';
+    } else if (sortHeader === 'Reviews') {
+      propertyName = 'reviewCount';
+    } else if (sortHeader === 'Code(s)') {
+      propertyName = 'codes';
+    }
+    setSortedRows(
+      rows.toSorted((a, b) =>
+        sortCompareFunction(a, b, propertyName, sortDirection)
+      )
+    );
+    setSortDirection(sortDirection);
+  };
 
   const formatNumber = (value) => {
-    return Number.isNaN(value) || typeof value === "undefined"
-      ? "N/A"
+    return Number.isNaN(value) || typeof value === 'undefined'
+      ? 'N/A'
       : value.toFixed(2);
-  }
+  };
+
+  const getAvg = (key1, key2) => {
+    let naCount = 0;
+    return formatNumber(
+      sortedRows.reduce((sum, row) => {
+        if (key1 === 'N/A' || key2 === 'N/A') {
+          naCount++;
+          return sum;
+        }
+        return key2 ? sum + row[key1] / row[key2] : sum + row[key1];
+      }, 0) /
+        sortedRows.length -
+        naCount
+    );
+  };
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <table className="table table-bordered" id={ tableId } >
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          <table className='table table-bordered' id={tableId}>
             <thead>
               <tr>
-                { showCheckbox && <th scope="col">Add</th> }
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Course {currentlySortedBy === "Course" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                {showCheckbox && <th scope='col'>Add</th>}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Course{' '}
+                  {currentlySortedBy === 'Course' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Rating {currentlySortedBy === "Rating" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Rating{' '}
+                  {currentlySortedBy === 'Rating' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Difficulty {currentlySortedBy === "Difficulty" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Difficulty{' '}
+                  {currentlySortedBy === 'Difficulty' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Workload {currentlySortedBy === "Workload" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Workload{' '}
+                  {currentlySortedBy === 'Workload' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Reviews {currentlySortedBy === "Reviews" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Reviews{' '}
+                  {currentlySortedBy === 'Reviews' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Foundational? {currentlySortedBy === "Foundational?" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Foundational?{' '}
+                  {currentlySortedBy === 'Foundational?' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Rating:Difficulty {currentlySortedBy === "Rating:Difficulty" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Rating:Difficulty{' '}
+                  {currentlySortedBy === 'Rating:Difficulty' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Rating:Workload {currentlySortedBy === "Rating:Workload" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Rating:Workload{' '}
+                  {currentlySortedBy === 'Rating:Workload' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
-                <th scope="col" onClick={ handleHeaderClick }>
-                  Code(s) {currentlySortedBy === "Code(s)" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                <th scope='col' onClick={handleHeaderClick}>
+                  Code(s){' '}
+                  {currentlySortedBy === 'Code(s)' ? (
+                    sortDirection == 'ascending' ? (
+                      <span className='arrow'>↑</span>
+                    ) : (
+                      <span className='arrow'>↓</span>
+                    )
+                  ) : null}
                 </th>
               </tr>
             </thead>
             <tbody>
-              { sortedRows.length === 0 ? (
-                <tr className="empty-table">
-                  <td>There's nothing here yet. Pick a specialization and add some classes to populate your course list!</td>
+              {sortedRows.length === 0 ? (
+                <tr className='empty-table'>
+                  <td>
+                    There's nothing here yet. Pick a specialization and add some
+                    classes to populate your course list!
+                  </td>
                 </tr>
-              ) : sortedRows.map(
-                (
-                  {
-                    id,
-                    slug,
-                    codes,
-                    isFoundational,
-                    name,
-                    officialURL,
-                    rating,
-                    difficulty,
-                    workload,
-                    reviewCount,
-                  },
-                  index
-                ) => (
-                  <tr
-                    key={id}
-                    className={index % 2 === 0 ? undefined : "bg-gray-50"}
-                  >
-                    { showCheckbox && <td>
-                      <input type="checkbox" className="course-checkbox" onChange={(event) => addToCourseList({
-                        id,
-                        slug,
-                        codes,
-                        isFoundational,
-                        name,
-                        officialURL,
-                        rating,
-                        difficulty,
-                        workload,
-                        reviewCount,
-                      })}/>
-                    </td> }
-                    <td>
-                      <div>{ name }</div>
-                      <div>
-                        <a href={officialURL} target="_blank" rel="noreferrer">GT Official</a> - <a href={"https://www.omscentral.com/courses/" + slug + "/reviews"} target="_blank" rel="noreferrer">Reviews</a>
-                      </div>
-                    </td>
-                    <td>{ formatNumber(rating) }</td>
-                    <td>{ formatNumber(difficulty) }</td>
-                    <td>{ formatNumber(workload) }</td>
-                    <td>{ reviewCount }</td>
-                    <td>{ isFoundational ? "yes" : "no" }</td>
-                    <td>{ formatNumber(rating / difficulty) }</td>
-                    <td>{ formatNumber(rating / workload) }</td>
-                    <td>{ codes.join(', ') }</td>
-                  </tr>
+              ) : (
+                sortedRows.map(
+                  (
+                    {
+                      id,
+                      slug,
+                      codes,
+                      isFoundational,
+                      name,
+                      officialURL,
+                      rating,
+                      difficulty,
+                      workload,
+                      reviewCount,
+                    },
+                    index
+                  ) => (
+                    <tr
+                      key={id}
+                      className={index % 2 === 0 ? undefined : 'bg-gray-50'}
+                    >
+                      {showCheckbox && (
+                        <td>
+                          <input
+                            type='checkbox'
+                            className='course-checkbox'
+                            onChange={(event) =>
+                              addToCourseList({
+                                id,
+                                slug,
+                                codes,
+                                isFoundational,
+                                name,
+                                officialURL,
+                                rating,
+                                difficulty,
+                                workload,
+                                reviewCount,
+                              })
+                            }
+                          />
+                        </td>
+                      )}
+                      <td>
+                        <div>{name}</div>
+                        <div>
+                          <a
+                            href={officialURL}
+                            target='_blank'
+                            rel='noreferrer'
+                          >
+                            GT Official
+                          </a>{' '}
+                          -{' '}
+                          <a
+                            href={
+                              'https://www.omscentral.com/courses/' +
+                              slug +
+                              '/reviews'
+                            }
+                            target='_blank'
+                            rel='noreferrer'
+                          >
+                            Reviews
+                          </a>
+                        </div>
+                      </td>
+                      <td>{formatNumber(rating)}</td>
+                      <td>{formatNumber(difficulty)}</td>
+                      <td>{formatNumber(workload)}</td>
+                      <td>{reviewCount}</td>
+                      <td>{isFoundational ? 'yes' : 'no'}</td>
+                      <td>{formatNumber(rating / difficulty)}</td>
+                      <td>{formatNumber(rating / workload)}</td>
+                      <td>{codes.join(', ')}</td>
+                    </tr>
                   )
-                )}
+                )
+              )}
+
+              {tableId === 'chosenCourses' && sortedRows.length !== 0 && (
+                <tr className='averages-row'>
+                  <td>
+                    <div>Average Value</div>
+                  </td>
+                  <td>{getAvg('rating')}</td>
+                  <td>{getAvg('difficulty')}</td>
+                  <td>{getAvg('workload')}</td>
+                  <td>{getAvg('reviewCount')}</td>
+                  <td></td>
+                  <td>{getAvg('rating', 'difficulty')}</td>
+                  <td>{getAvg('rating', 'workload')}</td>
+                  <td></td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
       </div>
-  </div>
-  )
+    </div>
+  );
 }
 
 export default BasicTable;
